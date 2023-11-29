e8056489bfe4a59626f2ba4aa00744808311766b
akash2435 <106023339+akash2435@users.noreply.github.com>
2023-11-29 18:20:13 +0530

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,34 @@
+﻿[HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.Username);
+
+            if (user == null)
+            {
+                return Unauthorized(new Response { Status = "Error", Message = "Invalid username or password." });
+            }
+
+            // Check if the entered password is correct
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, loginModel.Password);
+
+            if (!isPasswordValid)
+            {
+                return Unauthorized(new Response { Status = "Error", Message = "Invalid username or password." });
+            }
+
+            if (user.TwoFactorEnabled)
+            {
+                await _signInManager.SignOutAsync();
+                       await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, true);
+                       var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+
+                       var message = new Message(new string[] { user.Email! }, "OTP Confrimation", token);
+                       _emailService.SendEmail(message);
+
+             }
+            return StatusCode(StatusCodes.Status200OK,
+                 new Response { Status = "Success", Message = $"We have sent an OTP to your Email {user.Email}" });
+
+
+        }
